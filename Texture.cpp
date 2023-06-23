993e3082ee26898431cd416547349b68df951084
央旺 佐藤 <220511@jc-21.jp>
2023-06-23 10:31:04 +0900

    Textureクラスの修正

--- Texture.cpp
+++ Texture.cpp
@@ -1,40 +1,37 @@
-#include <DirectXTex.h>
 #include "Texture.h"
 #include "Direct3D.h"
 
-#pragma comment(lib, "DirectXTex.lib")
+#include <DirectXTex.h>
 
-Texture::Texture() :
-	pSampler_(nullptr), pSRV_(nullptr), width_(0), height_(0), depth_(0)
+#pragma comment(lib,"DirectXTex.lib")
+
+
+Texture::Texture()
+	:pSampler_(nullptr), pSRV_(nullptr)
 {
-	Release();
 }
 
 Texture::~Texture() {
-
+	Release();
 }
 
-//画像の読み込み
-HRESULT Texture::Load(std::string fileName) {
-	HRESULT hr = S_OK;
+//ファイルの読み込み
+HRESULT Texture::Load(string filename){
+	using namespace DirectX;
+	//////////画像読み込み部分（変更）
 
 	wchar_t wtext[FILENAME_MAX];
 	size_t ret;
-	mbstowcs_s(&ret, wtext, fileName.c_str(), fileName.length());
+	mbstowcs_s(&ret, wtext, filename.c_str(), filename.length());
 
-	using namespace DirectX;
-	TexMetadata metadata;		//画像のデータを扱う構造体
-	ScratchImage image;			//画像そのものを扱うクラス（画像の本体）
+	TexMetadata metadata;
+	ScratchImage image;
+	HRESULT hr;
 	hr = LoadFromWICFile(wtext, WIC_FLAGS::WIC_FLAGS_NONE, &metadata, image);
-	//エラー処理
+	/////////
 	if (FAILED(hr)) {
-		MessageBox(nullptr, "画像の読み込みに失敗しました", "エラー", MB_OK);
-		return hr;
+		return E_FAIL;
 	}
-	width_ = metadata.width;
-	height_ = metadata.height;
-	depth_ = metadata.depth;
-
 	//サンプラーの作成
 	D3D11_SAMPLER_DESC  SamDesc;
 	ZeroMemory(&SamDesc, sizeof(D3D11_SAMPLER_DESC));
@@ -43,29 +40,28 @@ HRESULT Texture::Load(std::string fileName) {
 	SamDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
 	SamDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
 	hr = Direct3D::pDevice_->CreateSamplerState(&SamDesc, &pSampler_);
-	//エラー処理
 	if (FAILED(hr)) {
-		MessageBox(nullptr, "サンプラーの作成に失敗しました", "エラー", MB_OK);
-		return hr;
+		return E_FAIL;
 	}
 
-	//シェーダーリソースビューの作成
+	//シェーダーリソースビュー
 	D3D11_SHADER_RESOURCE_VIEW_DESC srv = {};
 	srv.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 	srv.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
 	srv.Texture2D.MipLevels = 1;
-	hr = CreateShaderResourceView(Direct3D::pDevice_, image.GetImages(), image.GetImageCount(),metadata, &pSRV_);
-	//エラー処理
+
+	hr = CreateShaderResourceView(Direct3D::pDevice_,
+		image.GetImages(), image.GetImageCount(), metadata, &pSRV_);
 	if (FAILED(hr)) {
-		MessageBox(nullptr, "シェーダーリソースビューの作成に失敗しました", "エラー", MB_OK);
-		return hr;
+		return S_FALSE;
 	}
+
 
 	return S_OK;
 }
 
-//解放
+//開放
 void Texture::Release() {
-	SAFE_RELEASE(pSRV_);
 	SAFE_RELEASE(pSampler_);
+	SAFE_RELEASE(pSRV_);
 }
--- Texture.h
+++ Texture.h
@@ -6,29 +6,17 @@
 using namespace Microsoft::WRL;
 using std::string;
 
-class Texture {
+class Texture
+{
 	ID3D11SamplerState* pSampler_;
 	ID3D11ShaderResourceView* pSRV_;
-
-	size_t width_;		//横幅
-	size_t height_;		//縦幅
-	size_t depth_;		//奥行
 public:
 	Texture();
 	~Texture();
-	//画像の読み込み
-	HRESULT Load(string fileName);
-	//解放
+	//ファイルの読み込み
+	HRESULT Load(string filename);
+	//開放
 	void Release();
-	//サンプラーのゲッター
-	ID3D11SamplerState* GetSampler() { return pSampler_; };
-	//シェーダーリソースビューのゲッター
-	ID3D11ShaderResourceView* GetSRV() { return pSRV_; };
-
-	//横幅のゲッター
-	size_t GetWidth() { return width_; };
-	//縦幅のゲッター
-	size_t GetHeight() { return height_; };
-	//奥行のゲッター
-	size_t GetDepth() { return depth_; };
+	ID3D11SamplerState* GetSampler() { return pSampler_; }
+	ID3D11ShaderResourceView* GetSRV() { return pSRV_; }
 };
