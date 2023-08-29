cdf0387bc5dfd1fb63bcf4e40e6a5b8c2e5a87c9
央旺 佐藤 <220511@jc-21.jp>
2023-08-29 14:22:48 +0900

    メモの追記

--- Controller.cpp
+++ Controller.cpp
@@ -38,62 +38,60 @@ void Controller::Update()
 		transform_.rotate_.x -= 2.0f;
 	}
 
+	//上下の角度の制限
 	if (transform_.rotate_.x >= 60.0f) transform_.rotate_.x = 60.0f;
 	if (transform_.rotate_.x <=  -25.0f) transform_.rotate_.x =  -25.0f;
 
 	//現在地情報をベクトル型に変換
 	XMVECTOR vPos = XMLoadFloat3(&transform_.position_);
 
-	//transform_.rotate_.y度回転させる行列
+	//回転を表す行列
 	XMMATRIX mRotY = XMMatrixRotationY(XMConvertToRadians(transform_.rotate_.y));
 	XMMATRIX mRotX = XMMatrixRotationX(XMConvertToRadians(transform_.rotate_.x));
 
 	//1フレーム当たりの移動量（ベクトル）
-	XMVECTOR vMove = { 0.0f, 0.0f, 0.1f, 0.0f };     //奥方向に0.1m
+	XMVECTOR vMoveFoward = { 0.0f, 0.0f, 0.1f, 0.0f };	//奥方向に0.1m
 
 	//1フレーム当たりの移動量（ベクトル）
-	XMVECTOR vSwipe = { 0.1f, 0.0f, 0.0f, 0.0f };     //横方向に0.1m
+	XMVECTOR vMoveRight = { 0.1f, 0.0f, 0.0f, 0.0f };	//横方向に0.1m
 
 	//移動ベクトルを変形（向いている方向）
-	vMove = XMVector3TransformCoord(vMove, mRotY);
-	vSwipe = XMVector3TransformCoord(vSwipe, mRotY);
+	vMoveFoward = XMVector3TransformCoord(vMoveFoward, mRotY);
+	vMoveRight = XMVector3TransformCoord(vMoveRight, mRotY);
 
 	//移動処理
-	if (Input::IsKey(DIK_W))
+	if (Input::IsKey(DIK_W))		//前
 	{
-		vPos += vMove;                              //移動
+		vPos += vMoveFoward;						//移動
 		XMStoreFloat3(&transform_.position_, vPos); //現在位置の更新
 	}
-	if (Input::IsKey(DIK_S))
+	if (Input::IsKey(DIK_S))		//後ろ
 	{
-		vPos -= vMove;                              //移動
+		vPos -= vMoveFoward;						//移動
+		XMStoreFloat3(&transform_.position_, vPos);	//現在位置の更新
+	}
+	
+	if (Input::IsKey(DIK_D))		//右
+	{
+		vPos += vMoveRight;							//移動
+		XMStoreFloat3(&transform_.position_, vPos); //現在位置の更新
+	}
+	if (Input::IsKey(DIK_A))		//左
+	{
+		vPos -= vMoveRight;							//移動
 		XMStoreFloat3(&transform_.position_, vPos); //現在位置の更新
 	}
 
-	if (Input::IsKey(DIK_D))
-	{
-		vPos += vSwipe;                             //移動
-		XMStoreFloat3(&transform_.position_, vPos); //現在位置の更新
-	}
-	if (Input::IsKey(DIK_A))
-	{
-		vPos -= vSwipe;                             //移動
-		XMStoreFloat3(&transform_.position_, vPos); //現在位置の更新
-	}
+	XMVECTOR vCam = { 0, 0, 0, 0 };		//カメラの位置ベクトル
+	XMVECTOR vTarget = { 0, 0, 0, 0 };	//カメラの焦点ベクトル
 
-	XMFLOAT3 camTarget = { 0, 0, 0 };
-	XMFLOAT3 camPos = { 0, 0, 0 };
-	XMVECTOR vCam = { 0, 0, 0, 0 };
-	XMVECTOR vTarget = { 0, 0, 0, 0 };
-
-	camTarget = transform_.position_;
 	vCam = { 0, 5, -10, 0 };
 	vCam = XMVector3TransformCoord(vCam, mRotX);
 	vCam = XMVector3TransformCoord(vCam, mRotY);
-	XMStoreFloat3(&camPos, vPos + vCam);
 
-	Camera::SetTarget(camTarget);
-	Camera::SetPosition(camPos);
+	//カメラを設定
+	Camera::SetTarget(transform_.position_);
+	Camera::SetPosition(vPos + vCam);
 }
 
 //描画
